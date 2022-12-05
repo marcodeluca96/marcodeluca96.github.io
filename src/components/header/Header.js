05826cebe3a0ffb92c1e350e3f8a68bee865fb91
Marco De Luca <marco.deluca@weavesrl.com>
2022-12-05 11:07:08 +0100

    added Mars font

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
     <header>
       <div className='container header__container'>
         <h5>Hello I'm</h5>
-        <h1>Marco De Luca</h1>
-        <h5 className='text-light'>FullStack Developer</h5>
+        <h1 className='fontMars'>Marco De Luca</h1>
+        <h5 className='text-light fontMars'>FullStack Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className='me'>
